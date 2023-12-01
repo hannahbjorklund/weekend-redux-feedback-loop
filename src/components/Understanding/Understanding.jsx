c7d4c1ea2c038a98a4df95349d5fb79e9e3bd77a
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-12-01 17:23:43 -0600

    Completed dispatch and form for page 2

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,18 +1,43 @@
 import React, {useState} from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 /**
  * Take user input number 1-5 on how well their understanding is,
  * dispatch.
  */
 export default function Understanding(){
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [understandingInput, setUnderstandingInput] = useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(understandingInput);
+        dispatch({
+            type: 'SET_UNDERSTANDING',
+            payload: understandingInput
+        })
+        history.push('/support');
+    }
+
     return(
         <>
             <h1>How well are you understanding the content?</h1>
-            <Link to ='/support'>
-                <button className='next'>NEXT</button>
-            </Link>
+            <div className='container'>
+                <form onSubmit={handleSubmit}>
+                    <label>Understanding?</label>
+                    <input 
+                        type="number"
+                        min={1}
+                        max={5}
+                        onChange={(evt)=>{setUnderstandingInput(evt.target.value)}}
+                        value={understandingInput}
+                        data-testid="input"
+                    />
+                    <button type='submit' className='next' data-testid='next'>NEXT</button>
+                </form>
+            </div>
         </>
     )
 }
