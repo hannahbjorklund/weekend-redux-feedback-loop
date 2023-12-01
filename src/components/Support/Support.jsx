71daaf7985cb5d2ac85f228a1cbbb241970c11cd
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-12-01 17:31:07 -0600

    Finished dispatch and form for support page

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,18 +1,42 @@
 import React, {useState} from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 /**
  * Take user input number 1-5 for how well supported they feel,
  * dispatch to the feedback reducer
  */
 export default function Support(){
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [supportInput, setSupportInput] = useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(supportInput);
+        dispatch({
+            type: 'SET_SUPPORT',
+            payload: supportInput
+        })
+        history.push('/comments');
+    }
     return (
         <>
             <h1>How well are you being supported?</h1>
-            <Link to ='/comments'>
-                <button className='next'>NEXT</button>
-            </Link>
+            <div className='container'>
+                <form onSubmit={handleSubmit}>
+                    <label>Support?</label>
+                    <input 
+                        type="number"
+                        min={1}
+                        max={5}
+                        onChange={(evt)=>{setSupportInput(evt.target.value)}}
+                        value={supportInput}
+                        data-testid="input"
+                    />
+                    <button type='submit' className='next' data-testid='next'>NEXT</button>
+                </form>
+            </div>
         </>
     )
 }
